ad1ac0a7407b1bd0d678653d1481983f18d924c2
Vernon Mensah <vernonmensah@gmail.com>
2018-02-16 00:17:11 -0500

    can see events in campaign view

--- src/components/Dashboard_Campaign_View.js
+++ src/components/Dashboard_Campaign_View.js
@@ -22,25 +22,46 @@ export class DashboardCampaignView extends Component {
     }
 
     render() {
+
+        let loading;
+        if (this.props.loading) {
+            loading = <h3>Loading...</h3>;
+        }
+
+        let campaign_view;
+        if (!this.props.loading && this.props.campaignEvent && !this.props.message) {
+            campaign_view =
+                <div>
+                    <h1> Campaign: {this.props.campaign.name} </h1>
+                    <div>
+                        <div className='box'>
+                            <h2>Subscribers: {this.props.campaignEvent.contacts.length} </h2>
+                        </div>
+                        <div className='box'>
+                            <h2>Delivery: {this.props.campaignEvent.delivery.emails.length} </h2>
+                        </div>
+                        <div className='box'>
+                            <h2>Open: {this.props.campaignEvent.open.emails.length} </h2>
+                        </div>
+                        <div className='box'>
+                            <h2>Click: {this.props.campaignEvent.click.emails.length}</h2>
+                        </div>
+                    </div>
+                </div>;
+        }
+
+        let error;
+        if (this.props.message) {
+            error = <h3>{this.props.message}</h3>;
+        }
+
         console.log('THIS.PROPS', this.props);
 
         return(
             <section>
-                <h1> Campaign: {this.props.campaign.name} </h1>
-                <div>
-                    <div className='box'>
-                        <h2>Subscribers</h2>
-                    </div>
-                    <div className='box'>
-                        <h2>Delivery</h2>
-                    </div>
-                    <div className='box'>
-                        <h2>Open</h2>
-                    </div>
-                    <div className='box'>
-                        <h2>Click</h2>
-                    </div>
-                </div>
+                {loading}
+                {error}
+                {campaign_view}
             </section>
         );
     }

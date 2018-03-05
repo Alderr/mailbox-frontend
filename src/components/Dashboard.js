20a3881cfa26a02be2c453727b41a345d2ca8ec4
Vernon Mensah <vernonmensah@gmail.com>
2018-03-04 23:59:03 -0500

    some refactoring

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,9 +8,6 @@ import { getDashboard } from '../actions/userActions';
 
 
 export class Dashboard extends Component {
-    constructor(props){
-        super(props);
-    }
 
     componentWillMount() {
         this.props.dispatch(getDashboard(this.props.userId));
--- src/components/MainView.js
+++ src/components/MainView.js
@@ -6,12 +6,6 @@ import loginGate from './requires-login-gate';
 
 
 export class MainView extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    componentWillMount() {
-    }
 
     render() {
 

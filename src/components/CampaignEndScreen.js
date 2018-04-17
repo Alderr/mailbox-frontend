0c88a087ef9340ba2921cfc4dabf537f8e86e431
Vernon Mensah <vernonmensah@gmail.com>
2018-04-17 17:47:05 -0400

    endScreen is now just a func

--- src/components/CampaignEndScreen.js
+++ src/components/CampaignEndScreen.js
@@ -1,42 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import loginGate from './requires-login-gate';
 
-export class CampaignEndScreen extends Component {
-    constructor(props){
-        super(props);
+export function CampaignEndScreen(props) {
 
-        this.state = {
-            name: '',
-            subject: '',
-            listsSelected: [],
-        };
-
-    }
-
-    saveDataAndMoveToCampaignScreen() {
-        this.props.sendCampaignData();
-        this.props.history.push('/dashboard/campaigns');
-    }
-
-    render() {
-        console.log('PROPS!', this.props);
-
-        return(
-            <div>
-                <h1> One Click & Send The Campaign </h1>
-                <button onClick={() => this.props.sendCampaignData()}> Send ^^ </button>
-            </div>
-        );
-
-
-    }
+    return(
+        <div>
+            <h1> One Click & Send The Campaign </h1>
+            <button onClick={() => props.sendCampaignData()}> Send ^^ </button>
+        </div>
+    );
 }
 
-const mapStateToProps = Reducers => {
-    return {
-        lists: Reducers.userReducer.listsData
-    };
-};
-
-export default loginGate()(connect(mapStateToProps)(CampaignEndScreen));
+export default loginGate()(CampaignEndScreen);

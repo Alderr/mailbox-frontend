8df4e9da9ac678865f3a4b0638ae4fa4c4ed49a8
Vernon Mensah <vernonmensah@gmail.com>
2018-11-22 22:30:54 -0500

    move tons of shite around

--- src/components/CampaignPage/components/CampaignItem/CampaignItem.js
+++ src/components/CampaignPage/components/CampaignItem/CampaignItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-import { deleteCampaign } from '../../actions/userActions';
+import { deleteCampaign } from '../../../actions/userActions';
+
+import './CampaignItem.css';
 
 export function CampaignItem(props) {
 
     return(
-        <section>
+        <section className={'campaign-item-container'}>
             <li>
                 <Link to={`/dashboard/campaigns/id/${props.id}`}>
-                    {props.campaignName} - Sent on {props.date}
+                    {props.campaignName} - Sent on {moment(props.date).format('MMMM Do YYYY, h:mm:ss a')}
                 </Link>
                 {props.crud ? <button onClick={() => props.dispatch(deleteCampaign(props.userId, props.id))}>Delete</button> : null}
             </li>
